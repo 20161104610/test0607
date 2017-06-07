67dd3534532eeab1b7eb7db705862be64f6bbdf2
Stu06Han <2357706458@qq.com>
2017-06-07 08:45:18 +0800

    Initial Commit

---
+++ test0607/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test0607
+//
+//  Created by 20161104610 on 17/6/7.
+//  Copyright © 2017年 20161104610. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
